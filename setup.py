21b5a2f0746de6022637756a42123af6143a2130
Luke Benstead <lukeb@potatolondon.com>
2018-12-10 15:39:33 +0000

    Fix issue with setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+
+DESC = """
+    Django Database Backends for Google Cloud Databases
+"""
 
 setup(
     name='django-gcloud-connectors',
     version='0.1',
     packages=['gcloudc'],
     description='Django Database Backends for Google Cloud Databases',
-    long_description=README,
+    long_description=DESC,
     author='Potato London Ltd.',
     author_email='mail@p.ota.to',
     url='https://github.com/potatolondon/django-gcloud-connectors/',
