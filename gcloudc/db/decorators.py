b9bad3fc0e788f8cc61aa340d6ccb8f3fd9b8ebc
Alessandro Artoni <artoale@potatolondon.com>
2019-12-19 17:13:32 +0000

    Allow to disable cache, reimplement UniqueQuery

--- gcloudc/db/decorators.py
+++ gcloudc/db/decorators.py
@@ -1,15 +1,19 @@
 
 
+from gcloudc.db.backends.datastore import caching
 from contextlib import ContextDecorator
 import logging
 
 
 class DisableCache(ContextDecorator):
     def __enter__(self):
+        self.context = caching.get_context()
+        self.context.context_enabled = False
         logging.warn("disable_cache NOT IMPLEMENTED YET")
         return self
 
     def __exit__(self, *args, **kwargs):
+        self.context.context_enabled = True
         return False
 
 
