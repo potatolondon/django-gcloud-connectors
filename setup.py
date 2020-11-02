3dcd437298e6418cd0efdedb049c9e8cce01a620
Luke Benstead <kazade@gmail.com>
2020-11-02 13:59:58 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ AUTHOR_EMAIL = "mail@p.ota.to"
 if os.environ.get('CI_COMMIT_TAG'):
     VERSION = os.environ['CI_COMMIT_TAG']
 else:
-    VERSION = '0.2.6'
+    VERSION = '0.2.8'
 
 setup(
     name=NAME,
